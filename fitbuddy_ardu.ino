ed7e36f2e6a3fde7b17e963a65ab853c181e5b47
Ozge Fedai <ozgefedai@gmail.com>
2022-12-06 00:29:45 +0100

    Create fitbuddy_ardu.ino

---
+++ fitbuddy_ardu.ino
@@ -0,0 +1,137 @@
+/*include libraries*/
+#include <Wire.h>
+
+
+//Heart beat sensor
+int PulseSensorPurplePin = A0;       // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
+int LED13 = 13;   //  The on-board Arduion LED
+//light sensor
+int light_sensor = A1;
+int LM35_temp = A3;
+int THERMISTOR = A2;
+
+
+int Signal;                // holds the incoming raw data. Signal value can range from 0-1024
+int Threshold = 1;            // Determine which Signal to "count as a beat", and which to ingore.
+
+
+char string_temp[32];
+char write_buffer[256];
+
+unsigned long prev_micros = 0;
+void setup() {
+    Serial.begin(9600);
+    // Begin I2c
+
+    pinMode(LED13,INPUT);
+    Wire.begin();
+}
+ 
+
+void loop() {
+
+    //----------------------for the heartbeat sensor --------------------------------------------------
+    float Signal = analogRead(PulseSensorPurplePin);
+    //Serial.println(Signal);   
+    dtostrf(Signal,6,4,string_temp);
+    sprintf(write_buffer, "%c %s",'H',string_temp);
+
+    if(Signal > Threshold){                          // If the signal is above "550", then "turn-on" Arduino's on-Board LED.
+     digitalWrite(LED13,HIGH);
+    } else {
+     digitalWrite(LED13,LOW);                //  Else, the sigal must be below "550", so "turn-off" this LED.
+    }
+    dtostrf(Signal,6,4,string_temp);
+    sprintf(write_buffer, "%s %s",write_buffer,string_temp);
+    dtostrf(Signal,6,4,string_temp);
+    sprintf(write_buffer, "%s %s",write_buffer,string_temp);
+    /*dtostrf(0,6,4,string_temp);
+    sprintf(write_buffer, "%s %s",write_buffer,string_temp);
+    dtostrf(0,6,4,string_temp);
+    sprintf(write_buffer, "%s %s",write_buffer,string_temp);
+
+    */
+    Wire.beginTransmission(0x1A);
+    Wire.write(write_buffer);
+    Wire.endTransmission(true);
+    //---------------------------------------------------------------------------------------------------------
+    //--------------------------------------light sensor-------------------------------------------------------
+
+
+      
+    //float val;
+    float light = analogRead(light_sensor);
+    float light_ambient =0;//const int
+    float dark_ambient = 2000;
+    //Serial.println(light);
+    //val = map(light, light_ambient, dark_ambient, 0, 600);//(-50, 100)before
+
+    dtostrf(light ,6, 4, string_temp);
+    sprintf(write_buffer, "%c %s", 'L', string_temp);
+
+    dtostrf(0,6,4,string_temp);
+    sprintf(write_buffer, "%s %s",write_buffer,string_temp);
+    dtostrf(0,6,4,string_temp);
+    sprintf(write_buffer, "%s %s",write_buffer,string_temp);
+      
+
+    
+    //send 
+
+    Wire.beginTransmission(0x1A);
+    Wire.write(write_buffer);
+    Wire.endTransmission(true);
+
+    //-------------------------------------lm35-----------------------------------
+
+    float LM35 = analogRead(LM35_temp);
+
+    dtostrf(LM35 ,6, 4, string_temp);
+    sprintf(write_buffer, "%c %s", 'T', string_temp);
+    //Serial.println(LM35);
+    dtostrf(0,6,4,string_temp);
+    sprintf(write_buffer, "%s %s",write_buffer,string_temp);
+    dtostrf(0,6,4,string_temp);
+    sprintf(write_buffer, "%s %s",write_buffer,string_temp);
+      
+
+    
+    //send 
+
+    Wire.beginTransmission(0x1A);
+    Wire.write(write_buffer);
+    Wire.endTransmission(true);
+
+        //-------------------------------------thermistor-----------------------------------
+
+    float thermistor = analogRead(THERMISTOR);
+
+    dtostrf(thermistor ,6, 4, string_temp);
+    sprintf(write_buffer, "%c %s", 'R', string_temp);
+    Serial.println(thermistor);
+    dtostrf(0,6,4,string_temp);
+    sprintf(write_buffer, "%s %s",write_buffer,string_temp);
+    dtostrf(0,6,4,string_temp);
+    sprintf(write_buffer, "%s %s",write_buffer,string_temp);
+      
+
+    
+    //send 
+
+    Wire.beginTransmission(0x1A);
+    Wire.write(write_buffer);
+    Wire.endTransmission(true);
+
+
+
+
+
+    delay(100);
+}
+
+
+
+
+
+
+
